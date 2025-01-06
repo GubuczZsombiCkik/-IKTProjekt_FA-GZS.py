ffa33cfa044d063a5aef1bab7ea35e87877fb48f
Zsombiszn <gubuczzsombor21@gmail.com>
2025-01-06 17:55:00 +0100

    1

---
+++ AF_GZS_IKTP.py
@@ -0,0 +1,16 @@
+# Fulop Aron, Gubucz Zsombor Python első beadandó
+
+from termcolor import colored
+import random
+
+
+mlist=[101, 138, 112, 121, 176, 163, 123, 210, 226]
+
+menu = [
+    ("Küszöbök számolása"),
+    ("Hegyek (hegycsúcsok) számolása"),
+    ("Nyereg pont keresése"),
+    ("Legnagyobb szintkülönbség keresése"),
+    ("Lista módosítása (hozzáadás, törlés, módosítás)"),
+    ("Kilépés!"),
+]
